a32e3b0a2709f06cf9dfd50b53895200ddd5694d
Yazir <alexishernandezgt05@gmail.com>
2024-06-20 23:35:27 -0600

    segundo envio

--- PRACTICA_3/MontañaRusa.php
+++ PRACTICA_3/MontañaRusa.php
@@ -7,7 +7,11 @@
         echo "<h2>Debe aceptar no llevarnos a juicio por daños y perjuicios de un mal mantenimiento.</h2>";
     }
     else{
-        echo "<h2>Felicidades, aqui tiene tu tiket</h2>";
+        echo "<form>";
+        echo "<h2>Hola, Felicidades<br> Cumples  con los requisitos para ingresar a la montaña rusa</h2>";
+        echo "<h2>Su estaturan es ".$estatura."cm</h2>";
+        echo "<h2>Aqui tiene su tiket</h2>";
         echo "<img src='imagen/tiket.jpg' alt='Ticket'>";
+        echo "</form>";
     }
 ?>
