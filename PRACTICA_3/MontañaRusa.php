ead2311400bc583c1040d0662f4d543ef56a1f62
Yazir <alexishernandezgt05@gmail.com>
2024-06-21 07:09:55 -0600

    actualizacion

--- PRACTICA_3/MontañaRusa.php
+++ PRACTICA_3/MontañaRusa.php
@@ -1,4 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap 5-->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"/>
+      <!--Script JS-->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous">
+    </script>
+    <title>Tiket</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
     $estatura = $_POST['estatura'];
     $edad = $_POST['edad'];
     $terminos = $_POST['terminos'];
@@ -7,11 +28,13 @@
         echo "<h2>Debe aceptar no llevarnos a juicio por daños y perjuicios de un mal mantenimiento.</h2>";
     }
     else{
-        echo "<form>";
-        echo "<h2>Hola, Felicidades<br> Cumples  con los requisitos para ingresar a la montaña rusa</h2>";
+        echo "<form class='container'>";
+        echo "<h1>Hola, Felicidades<br> Cumples  con los requisitos para ingresar a la montaña rusa</h1>";
         echo "<h2>Su estaturan es ".$estatura."cm</h2>";
         echo "<h2>Aqui tiene su tiket</h2>";
         echo "<img src='imagen/tiket.jpg' alt='Ticket'>";
         echo "</form>";
     }
 ?>
+</body>
+</html>
